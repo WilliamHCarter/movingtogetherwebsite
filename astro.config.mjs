64c2589274e29b2652603688451d7707bf70d5f1
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-03-11 03:45:28 -0500

    ✨: Mobile Menu v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [image()],
-//   site: "movinghomestogether.com",
+  integrations: [image(), tailwind(), preact()]
+  //   site: "movinghomestogether.com",
 });
