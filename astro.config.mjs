71d7310215835ba90b2757d77f54e685d4e32736
WilliamHCarter <41378465+WilliamHCarter@users.noreply.github.com>
2023-03-11 04:24:29 -0500

    💚: Fix Integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,8 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import preact from "@astrojs/preact";
-
-// https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [image(), tailwind(), preact()]
+  integrations: [image(), tailwind()]
   //   site: "movinghomestogether.com",
 });
